727569203b4286c46028de4acca57b72f9aa2284
Muhammad yaseen <ykcaptain2223@gmail.com>
2026-07-10 10:11:25 +0500

    this is my first file in python
    first three lecture practices

---
+++ Muhammad-Yaseen/first.py
@@ -0,0 +1,15 @@
+# print("hello world")
+
+print("in python we write  text like this   if we write number here it will excute  like text for example if i write 5+5 it give me 5+5  if i wrtie in another  ")
+print("if i write like this it work perfect like calculate ", 5+5 ,"it show perfect result  ")
+print("  #  if we write # in start of line for this line will be comments   mostly devloper use it to remind his code   are function of code ")
+
+
+# print("   if we write  in start of line for this line will be comments   mostly devloper use it to remind his code   are function of code ")
+
+# now if i want to  use multple lines for  write code\
+
+
+print(" i just start AI course ")
+print(" python is the first step of AI  ", end="")
+print(" and this much fun ")
