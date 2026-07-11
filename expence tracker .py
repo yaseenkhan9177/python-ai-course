d02d09f08c329c6c823cee71986e6b2a3a5d7c3c
sahilkhan-91 <uubaidullah753@gmail.com>
2026-07-11 11:12:28 -0700

    Add files via upload

---
+++ expence tracker .py
@@ -0,0 +1,9 @@
+# Daily budget and expense tracker
+total_budget=5000 #storing integer type
+food_expense =550.60
+travel_expense =110.70
+extra_expense =75
+total_expense = travel_expense +extra_expense+food_expense
+remaining_balance = total_budget - total_expense
+print("Total_expenses:",total_expense)
+print("remaining balance:",remaining_balance)
