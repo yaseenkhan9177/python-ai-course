0afc659096ef880c7e8ed45a2da58bf31d1ddabb
Muhammad yaseen <ykcaptain2223@gmail.com>
2026-07-11 17:10:42 +0500

    Add files via upload

---
+++ Muhammad-Yaseen/expense_tracker.py
@@ -0,0 +1,15 @@
+#daily buget and expanse tracker
+
+total_budget =10000  #storing integer type
+
+food_expense = 2000.0
+travel_expense = 3000.0
+extra_expense = 1500.0
+
+total_expense = food_expense + travel_expense + extra_expense
+
+remaining_blance = total_budget - total_expense
+
+#reaming money
+print("total Expenses : ",total_expense)
+print("remaining blance : ", remaining_blance)
---
+++ Muhammad-Yaseen/izhar first assigment.py
@@ -0,0 +1,35 @@
+#    part 1    what is the differeance between syntax 5+5 and code 5+5
+
+
+print("5+5")
+# it show the output 5+5
+
+#this is code  of 5+5
+print(5+5)
+# it show 10 in output
+
+
+
+
+#part 2 in part 2 if we use print ()    and the we use print() it show the output in next lines    but if we want ot show the output in one line we use the end="" praameter
+#part 2 example
+print("hi my name is ")
+print("khan ")
+
+
+print("hi my name is ",end=" ")
+print("khan")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print(15*10)
+
+# 4 codeing challenages    bio print
+print("my name is yaseen")
+print("my age is ",21,)
+print("my favt programing language is python")
+
+
