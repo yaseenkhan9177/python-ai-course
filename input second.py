c5bf3a0ca43f72b9cb770610c6595f2c74f6ae97
sahilkhan-91 <uubaidullah753@gmail.com>
2026-07-13 11:41:08 -0700

    Add files via upload

---
+++ input second.py
@@ -0,0 +1,6 @@
+# cafe bill calculator
+price= float(input("enter the price of one coffe:"))
+quantity= int(input("enter the quantity of one cofee:"))
+#calculation
+total_bill= price*quantity
+print ("total bill=",total_bill)
